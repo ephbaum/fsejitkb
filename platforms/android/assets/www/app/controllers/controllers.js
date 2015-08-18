881f606a56511b284a550bfdaf3071807c55e325
unknown <kirschbs@MSP-kirschbs-w2.go.mts.com>
2015-08-18 16:42:46 -0500

    initial commit

---
+++ platforms/android/assets/www/app/controllers/controllers.js
@@ -0,0 +1,55 @@
+angular.module( 'app.controllers', [] )
+
+.controller('LoginCtrl', function ($scope, $state, $http, $localstorage) {
+
+  var storedUser = $localstorage.getObject( 'user' );
+  if ( typeof storedUser.username !== 'undefined' && typeof storedUser.password !== 'undefined' ) {
+    $http
+      .post( 'https://dev.mtsecho.com/knowledge/login', storedUser )
+      .then( function ( response ) {
+        console.dir( ['Success', response] );
+        $state.go( 'tab.dashboard' );
+      }, function ( error ) {
+        console.error( 'Error', error );
+    } );
+   }
+
+  $scope.login = function (user) {
+
+    console.dir( ['Login', user] );
+
+    $http
+      .post( 'https://dev.mtsecho.com/knowledge/login', user )
+      .then( function ( response ) {
+        console.dir( ['Success', response] );
+        $localstorage.setObject( 'user', user );
+        $state.go( 'tab.dashboard' );
+      }, function ( error ) {
+       console.error( 'Error', error );
+    });
+
+  };
+})
+
+.controller( 'DashboardCtrl', function ( $scope ) {
+  
+} )
+
+.controller( 'BookmarksCtrl', function ( $scope, Bookmarks ) {
+  $scope.bookmarks = Bookmarks.all();
+  $scope.remove = function( bookmark ) {
+    Bookmarks.remove( bookmark );
+  }
+})
+
+.controller( 'BookmarksDetailCtrl', function( $scope, $stateParams, Bookmarks ) {
+  $scope.bookmark = Bookmarks.get( $stateParams.bookmarkId );
+})
+
+.controller( 'SettingsCtrl', function( $scope ) {
+  $scope.settings = {
+    enablePush: true,
+    enableSMS: false,
+    enableEmail: false
+  };
+});
---
+++ www/app/services/services.js
@@ -0,0 +1,62 @@
+angular.module('app.services', [])
+
+.factory('Bookmarks', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var bookmarks = [{
+    id: 0,
+    title: 'Question One',
+    question: 'Why is the sky blue?',
+  }, {
+    id: 1,
+    title: 'Question Two',
+    question: 'Where are my car keys?',
+  }, {
+    id: 2,
+    title: 'Question Three',
+    question: 'Where are your car keys?',
+  }, {
+    id: 3,
+    title: 'Question Four',
+    question: 'Who am I?',
+  }, {
+    id: 4,
+    title: 'Question Five',
+    question: 'What am I doing here?',
+  }];
+
+  return {
+    all: function() {
+      return bookmarks;
+    },
+    remove: function( bookmark )  {
+      bookmarks.splice( bookmarks.indexOf( bookmark ), 1);
+    },
+    get: function( bookmarkId ) {
+      for ( var i = 0; i < bookmarks.length; i++ ) {
+        if ( bookmarks[i].id === parseInt( bookmarkId ) ) {
+          return bookmarks[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+.factory('$localstorage', ['$window', function ( $window ) {
+  return {
+    set: function ( key, value ) {
+      $window.localStorage[key] = value;
+    },
+    get: function ( key, defaultValue ) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function ( key, value ) {
+      $window.localStorage[key] = JSON.stringify( value );
+    },
+    getObject: function ( key ) {
+      return JSON.parse( $window.localStorage[key] || '{}' );
+    }
+  }
+}]);
